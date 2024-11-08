fc914d2ff5b0acbe6dd4b806855603fa938e67f1
Matheus torrico <mathues-torrico@outlook.com>
2024-11-08 08:22:56 +0100

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7___OOP_Generic_collections
+{
+    public class Employee
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+
+
+        public Employee(string ID, string Name, string Gender, int Salary)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Gender = Gender;
+            this.Salary = Salary;
+        }
+
+        public void PrintValues()
+        {
+            Console.WriteLine($"ID: {ID}, Name: {Name}, Gender: {Gender}, Salary: {Salary}");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab_7___OOP_Generic_collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee ANNA = new Employee ("F99","Anna","Female",3000);
+
+            Employee HÅKAN = new Employee("M75", "Håkan", "Male",2500);
+
+            Employee ANDERS = new Employee("M99", "Ander", "Male", 4000);
+
+            Employee THOMAS = new Employee("M44", "Thoms", "Male", 3400);
+
+            Employee MARIAN = new Employee("F77", "Marian", "Female", 5000); 
+
+            Stack<Employee> stack = new Stack<Employee>();
+
+            //Pushad alla objects i stacken
+            stack.Push(ANNA); 
+            stack.Push(ANDERS); 
+            stack.Push(ANDERS); 
+            stack.Push(THOMAS); 
+            stack.Push(MARIAN);
+
+            //Temporär lista
+            List<Employee> poppedEmployeesList = new List<Employee>();
+
+            //Skriver ut alla objekt med hjälp med metoden PrintValues
+            foreach (var item in stack)
+            {
+                item.PrintValues();
+                Console.WriteLine($"Item left in the stack: {stack.Count} ");
+            }
+
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+            Console.WriteLine("Retrive Using POP Method");
+
+            while (stack.Count > 0)
+            {
+      
+                Employee poppedEmployee = stack.Pop();
+                poppedEmployee.PrintValues();
+
+                Console.WriteLine($"Antalet objekts: {stack.Count} ");
+
+
+
+                poppedEmployeesList.Add(poppedEmployee);
+            }
+
+            Console.WriteLine("\nLägger till alla objekt igen i stacken med Push:");
+            foreach (Employee employee in poppedEmployeesList)
+            {
+                stack.Push(employee);
+            }
+
+            Console.WriteLine("\nskriver ut objekten");
+            foreach (var item in stack)
+            {
+                item.PrintValues();
+                Console.WriteLine($"Antalet objekts: {stack.Count} ");
+            }
+
+            Console.WriteLine("Retrive ussing Peek");
+
+            Employee firstPeeked = stack.Peek();
+            firstPeeked.PrintValues();
+            Console.WriteLine($"Items left in stack = {stack.Count}");
+
+            firstPeeked =stack.Pop();
+
+            Employee secoundPeeked = stack.Peek();
+
+            secoundPeeked.PrintValues();
+            Console.WriteLine($"Items left in stack = {stack.Count}");
+
+
+
+
+        }
+
+
+    }
+}
